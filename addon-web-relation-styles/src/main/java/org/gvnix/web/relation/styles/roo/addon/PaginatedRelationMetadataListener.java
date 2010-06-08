82d2e2099fd756c43f6e33bd4989b865bf773321
rgarcia <rgarcia@69435866-3869-0410-973d-9f951a852354>
2010-06-08 14:21:04 +0000

    Listener para metadatos JspMetada.
    Relacionado con el requisito [#278] para la tarea [T172].
    
    git-svn-id: http://scmcit.gva.es/svn/gvnix/trunk/code@92 69435866-3869-0410-973d-9f951a852354

---
+++ addon-web-relation-styles/src/main/java/org/gvnix/web/relation/styles/roo/addon/PaginatedRelationMetadataListener.java
@@ -0,0 +1,77 @@
+/*
+ * gvNIX. Spring Roo based RAD tool for Conselleria d'Infraestructures
+ * i Transport - Generalitat Valenciana
+ * Copyright (C) 2010 CIT - Generalitat Valenciana
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.gvnix.web.relation.styles.roo.addon;
+
+import java.util.logging.Logger;
+
+import org.apache.felix.scr.annotations.*;
+import org.osgi.service.component.ComponentContext;
+import org.springframework.roo.addon.mvc.jsp.JspMetadata;
+import org.springframework.roo.metadata.*;
+import org.springframework.roo.support.util.Assert;
+
+/**
+ * Listens for {@link WebScaffoldMetadata} and produces JSPs when requested by
+ * that metadata.
+ * 
+ * 
+ * @author Ricardo García Fernández( rgarcia at disid dot com ) at <a
+ *         href="http://www.disid.com">DiSiD Technologies S.L.</a> made for <a
+ *         href="http://www.cit.gva.es">Conselleria d'Infraestructures i
+ *         Transport</a>
+ */
+@Component(immediate = true)
+@Service
+public class PaginatedRelationMetadataListener implements // MetadataProvider,
+	MetadataNotificationListener {
+
+    @Reference
+    private MetadataDependencyRegistry metadataDependencyRegistry;
+
+    private static Logger logger = Logger.getLogger(PaginatedRelationMetadataListener.class.getName());
+
+    protected void activate(ComponentContext context) {
+	metadataDependencyRegistry.addNotificationListener(this);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.springframework.roo.metadata.MetadataNotificationListener#notify(
+     * java.lang.String, java.lang.String)
+     */
+    public void notify(String upstreamDependency, String downstreamDependency) {
+
+	if (MetadataIdentificationUtils
+		.isIdentifyingClass(downstreamDependency)) {
+	}
+
+	Assert.isTrue(MetadataIdentificationUtils.getMetadataClass(
+		upstreamDependency).equals(
+		MetadataIdentificationUtils.getMetadataClass(JspMetadata
+			.getMetadataIdentiferType())),
+		"Expected JspMetadata notifications (not '"
+			+ upstreamDependency + "')");
+
+	logger.warning("JspMetadata retrieved.");
+
+    }
+
+}
