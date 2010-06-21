df45d14362375e118690364e8e73f1cc8ab2b8eb
jmvivo <jmvivo@69435866-3869-0410-973d-9f951a852354>
2010-06-21 10:54:19 +0000

    [T171] Añadido addon-web-mvc-binding.
    git-svn-id: http://scmcit.gva.es/svn/gvnix/trunk/code@138 69435866-3869-0410-973d-9f951a852354

---
+++ addon-web-mvc-binding/src/main/java/org/es/gva/cit/web_binder/roo/addon/WebBinderFileListener.java
@@ -0,0 +1,65 @@
+package org.es.gva.cit.web_binder.roo.addon;
+
+import org.apache.felix.scr.annotations.*;
+import org.springframework.roo.classpath.PhysicalTypeIdentifier;
+import org.springframework.roo.classpath.operations.ClasspathOperations;
+import org.springframework.roo.file.monitor.event.FileEvent;
+import org.springframework.roo.file.monitor.event.FileEventListener;
+import org.springframework.roo.file.monitor.event.FileOperation;
+import org.springframework.roo.model.JavaType;
+import org.springframework.roo.project.Path;
+
+/**
+ * File Listener that checks any changes configuration files or Initializer java
+ * file.<br/>
+ * If this happens uses the operation class methods to update the configuration.
+ * 
+ * @author jmvivo
+ * 
+ */
+@Component
+@Service
+public class WebBinderFileListener implements FileEventListener {
+
+    @Reference
+    private WebBinderOperations webBinderOperations;
+    @Reference
+    private ClasspathOperations classpathOperations;
+
+    public void onFileEvent(FileEvent fileEvent) {
+
+	// Checks if is mvc config file
+	if (fileEvent.getFileDetails().getCanonicalPath().equals(
+		webBinderOperations.getPathToMvcConfig())) {
+	    if (fileEvent.getOperation() == FileOperation.UPDATED) {
+		webBinderOperations.clearCurrentIntializer();
+	    }
+	}
+
+	if (webBinderOperations.getCurrentInitializer() != null) {
+	    // Check if is java file of initialize
+	    String physicalTypeIdentifier = PhysicalTypeIdentifier
+		    .createIdentifier(webBinderOperations
+			    .getCurrentInitializer(), Path.SRC_MAIN_JAVA);
+	    String path = classpathOperations
+		    .getPhysicalLocationCanonicalPath(physicalTypeIdentifier);
+	    if (fileEvent.getFileDetails().getFile().getAbsolutePath().equals(
+		    path)) {
+		// It's the initilaizer file
+		if (fileEvent.getOperation() == FileOperation.DELETED) {
+		    // if it has been deleted this clear the configuration
+		    webBinderOperations.drop();
+		}
+
+	    } else if (fileEvent.getOperation() == FileOperation.RENAMED
+		    && fileEvent.getPreviousName().getAbsolutePath().equals(
+			    path)) {
+		// The initilaizer file has been renamed, update the mvc config
+		JavaType newType = null; // FIXME
+		webBinderOperations.renameInitializer(newType);
+	    }
+	}
+
+    }
+
+}
