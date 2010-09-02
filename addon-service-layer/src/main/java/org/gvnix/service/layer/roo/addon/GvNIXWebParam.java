52859a1155ae681901d4a980257a6febb4e4a446
rgarcia <rgarcia@69435866-3869-0410-973d-9f951a852354>
2010-09-02 09:22:53 +0000

    Requisito [#307] Add-on Capa de Servicios.
    Redefinir el paquete de la Anotación GvNIXWebParam
    
    git-svn-id: http://scmcit.gva.es/svn/gvnix/trunk/code@258 69435866-3869-0410-973d-9f951a852354

--- addon-service-layer/src/main/java/org/gvnix/service/layer/roo/addon/GvNIXWebParam.java
+++ addon-service-layer/src/main/java/org/gvnix/service/layer/roo/addon/GvNIXWebParam.java
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.gvnix.test.project.web.services.annotation;
+package org.gvnix.service.layer.roo.addon;
 
 import java.lang.annotation.*;
 
