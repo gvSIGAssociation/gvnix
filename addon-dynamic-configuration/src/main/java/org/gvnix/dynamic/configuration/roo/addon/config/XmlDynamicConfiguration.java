baa65ae7222323a7ac7c52a61f766fc6f94c843b
mmartinez <mmartinez@69435866-3869-0410-973d-9f951a852354>
2010-12-21 11:00:05 +0000

    Crear componente OSGi base para gestionar ficheros de configuración con formato XML, en esta primera versión solo realiza la lectura y solo considera los nodos del tipo texto del fichero XML origen.
    git-svn-id: http://scmcit.gva.es/svn/gvnix/trunk/code@479 69435866-3869-0410-973d-9f951a852354

--- addon-dynamic-configuration/src/main/java/org/gvnix/dynamic/configuration/roo/addon/config/XmlDynamicConfiguration.java
+++ addon-dynamic-configuration/src/main/java/org/gvnix/dynamic/configuration/roo/addon/config/XmlDynamicConfiguration.java
@@ -20,7 +20,8 @@ package org.gvnix.dynamic.configuration.roo.addon.config;
 
 import java.io.IOException;
 import java.util.Properties;
-import java.util.Map.Entry;
+
+import javax.xml.parsers.DocumentBuilder;
 
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Reference;
@@ -31,12 +32,17 @@ import org.springframework.roo.process.manager.FileManager;
 import org.springframework.roo.process.manager.MutableFile;
 import org.springframework.roo.project.Path;
 import org.springframework.roo.project.PathResolver;
+import org.springframework.roo.support.util.XmlUtils;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 /**
- * Dynamic configuration base manager of property files.
+ * Dynamic configuration base manager of XML files.
  * <p>
- * Extends this class adding the @DynComponent annotation to manage new
- * properties file values.
+ * Extends this class adding the @DynComponent annotation to manage new xml file
+ * values.
  * </p>
  * 
  * @author Mario Martínez Sánchez ( mmartinez at disid dot com ) at <a
@@ -46,7 +52,7 @@ import org.springframework.roo.project.PathResolver;
  */
 @Component
 @Service
-public class PropertiesDynamicConfiguration implements
+public class XmlDynamicConfiguration implements
     DefaultDynamicConfiguration {
   
   @Reference private PathResolver pathResolver;
@@ -58,23 +64,23 @@ public class PropertiesDynamicConfiguration implements
   public DynPropertyList read() {
 
     // Get the properties file path from the annotation
-    MutableFile file = getPropertiesFile();
+    MutableFile file = getXmlFile();
 
     DynPropertyList dynProps = new DynPropertyList();
-
     try {
 
-      Properties props = new Properties();
-      props.load(file.getInputStream());
-      for (Entry<Object, Object> prop : props.entrySet()) {
+      DocumentBuilder build = XmlUtils.getDocumentBuilder();
+      Document doc = build.parse(fileManager.getInputStream(file
+          .getCanonicalPath()));
+      dynProps.addAll(generateProperties("", doc.getChildNodes()));
+    }
+    catch (SAXException se) {
 
-        dynProps.add(new DynProperty(prop.getKey().toString(), prop
-            .getValue().toString()));
-      }
+      throw new IllegalStateException("Cant parse the XML file", se);
     }
     catch (IOException ioe) {
 
-      throw new IllegalStateException(ioe);
+      throw new IllegalStateException("Cant read the XML file", ioe);
     }
 
     return dynProps;
@@ -85,30 +91,48 @@ public class PropertiesDynamicConfiguration implements
    */
   public void write(DynPropertyList dynProps) {
 
-    try {
-
-      Properties props = new Properties();
-      for (DynProperty dynProp : dynProps) {
-
-        props.put(dynProp.getKey(), dynProp.getValue());
-      }
-
-      // Get the properties file path from the annotation
-      MutableFile file = getPropertiesFile();
-      props.store(file.getOutputStream(), null);
-    }
-    catch (IOException ioe) {
-
-      throw new IllegalStateException(ioe);
-    }
+    // TODO Construct the XML dynamic configuration write method
   }
 
   /**
-   * Get the properties mutable file from the annotation.
+   * Generate a dynamic property list from a list of XML nodes.
    * 
-   * @return Properties mutable file
+   * TODO Only TEXT_NODE nodes are considered, extend it.
+   * 
+   * @param baseName Parent node name of node list  
+   * @param nodes XML node list to convert
+   * @return Dynamic property list
    */
-  private MutableFile getPropertiesFile() {
+  private DynPropertyList generateProperties(String baseName, NodeList nodes) {
+
+    DynPropertyList dynProps = new DynPropertyList();
+
+    for (int i = 0; i < nodes.getLength(); i++) {
+
+      Node node = nodes.item(i);
+
+      short type = node.getNodeType();
+      String content = node.getTextContent();
+      String name = node.getNodeName();
+      NodeList childs = node.getChildNodes();
+      
+      dynProps.addAll(generateProperties(baseName + name, childs));
+      
+      if (type == Node.TEXT_NODE && content.trim().length() > 0) {
+
+        dynProps.add(new DynProperty(baseName, content));
+      }
+    }
+
+    return dynProps;
+  }
+
+  /**
+   * Get the XML mutable file from the annotation.
+   * 
+   * @return XML mutable file
+   */
+  private MutableFile getXmlFile() {
     
     DynamicConfiguration annotation = this.getClass().getAnnotation(
         DynamicConfiguration.class);
@@ -121,7 +145,7 @@ public class PropertiesDynamicConfiguration implements
     }
     else {
 
-      throw new IllegalStateException("Properties file not found");
+      throw new IllegalStateException("XML file not found");
     }
   }
 
