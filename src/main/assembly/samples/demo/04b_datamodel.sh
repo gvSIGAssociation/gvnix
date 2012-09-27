c25cd64a0526ddb15ca1adbab87bbfdf1a4b3516
eruiz <eruiz@69435866-3869-0410-973d-9f951a852354>
2012-09-27 09:01:01 +0000

    Added info about how to restore de sample db
    git-svn-id: http://scmcit.gva.es/svn/gvnix/trunk/code@1574 69435866-3869-0410-973d-9f951a852354

--- src/main/assembly/samples/demo/04b_datamodel.sh
+++ src/main/assembly/samples/demo/04b_datamodel.sh
@@ -9,3 +9,6 @@ createuser -W -D -P tiendavirtual
 
 createdb -E UTF-8 -O tiendavirtual tiendavirtualDB
 
+# Now you can load the database dump using command below:
+# pg_restore -v -h localhost -U tiendavirtual --no-owner -F c -d tiendavirtualDB 04b_datamodel.dump
+
