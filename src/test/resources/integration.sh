7f72cbb7b08aa0580b99f598a52ae83d71f61609
mmartinez <mmartinez@69435866-3869-0410-973d-9f951a852354>
2013-01-21 11:44:09 +0000

    #8216 Ejecutar los scripts de integración con la distribución construida de gvnix, en lugar de con el modo desarrollo gvnix-dev.
    git-svn-id: http://scmcit.gva.es/svn/gvnix/trunk/code@1700 69435866-3869-0410-973d-9f951a852354

--- src/test/resources/integration.sh
+++ src/test/resources/integration.sh
@@ -13,185 +13,185 @@ mkdir /tmp/gvnixtest$1
 cd /tmp/gvnixtest$1
 
 ## Roo bikeshop
-$2/code/bin/gvnix-dev script --file bikeshop.roo
+gvnix.sh script --file bikeshop.roo
 mvn test tomcat:run -Dmaven.tomcat.fork=true 
 rm -rf /tmp/gvnixtest$1/*
 
 ## Roo clinic
-$2/code/bin/gvnix-dev script --file clinic.roo
+gvnix.sh script --file clinic.roo
 mvn test tomcat:run selenium:xvfb selenium:selenese -Dmaven.tomcat.fork=true 
 rm -rf /tmp/gvnixtest$1/*
 
 ## Roo embedding
-$2/code/bin/gvnix-dev script --file embedding.roo
+gvnix.sh script --file embedding.roo
 mvn test tomcat:run -Dmaven.tomcat.fork=true 
 rm -rf /tmp/gvnixtest$1/*
 
 ## Roo expenses
-$2/code/bin/gvnix-dev script --file expenses.roo
+gvnix.sh script --file expenses.roo
 mvn test tomcat:run -Dmaven.tomcat.fork=true 
 rm -rf /tmp/gvnixtest$1/*
 
 ## Roo gae-expenses
-$2/code/bin/gvnix-dev script --file gae-expenses.roo
+gvnix.sh script --file gae-expenses.roo
 mvn test tomcat:run -Dmaven.tomcat.fork=true 
 rm -rf /tmp/gvnixtest$1/*
 
 ## Roo multimodule
-$2/code/bin/gvnix-dev script --file multimodule.roo
+gvnix.sh script --file multimodule.roo
 mvn test tomcat:run -Dmaven.tomcat.fork=true 
 rm -rf /tmp/gvnixtest$1/*
 
 ## Roo pizzashop
-$2/code/bin/gvnix-dev script --file pizzashop.roo
+gvnix.sh script --file pizzashop.roo
 mvn test tomcat:run -Dmaven.tomcat.fork=true 
 rm -rf /tmp/gvnixtest$1/*
 
 ## Roo vote
-$2/code/bin/gvnix-dev script --file vote.roo
+gvnix.sh script --file vote.roo
 mvn test tomcat:run -Dmaven.tomcat.fork=true 
 rm -rf /tmp/gvnixtest$1/*
 
 ## Roo wedding
-$2/code/bin/gvnix-dev script --file wedding.roo
+gvnix.sh script --file wedding.roo
 mvn test tomcat:run selenium:xvfb selenium:selenese -Dmaven.tomcat.fork=true 
 rm -rf /tmp/gvnixtest$1/*
 
 ## gvNIX binding add-on
-$2/code/bin/gvnix-dev script --file $2/code/addon-web-mvc-binding/src/main/resources/binding.roo
+gvnix.sh script --file $2/code/addon-web-mvc-binding/src/main/resources/binding.roo
 mvn test tomcat:run selenium:xvfb selenium:selenese -Dmaven.tomcat.fork=true 
 rm -rf /tmp/gvnixtest$1/*
 
 ## gvNIX configuration add-on
-$2/code/bin/gvnix-dev script --file $2/code/addon-dynamic-configuration/src/main/resources/configuration.roo
+gvnix.sh script --file $2/code/addon-dynamic-configuration/src/main/resources/configuration.roo
 mvn test tomcat:run selenium:xvfb selenium:selenese -Dmaven.tomcat.fork=true 
 rm -rf /tmp/gvnixtest$1/*
 
 ## gvNIX configuration add-on
-$2/code/bin/gvnix-dev script --file $2/code/addon-dynamic-configuration/src/main/resources/configuration.roo
+gvnix.sh script --file $2/code/addon-dynamic-configuration/src/main/resources/configuration.roo
 mvn test tomcat:run selenium:xvfb selenium:selenese -Dmaven.tomcat.fork=true 
 rm -rf /tmp/gvnixtest$1/*
 
 ## gvNIX dialog add-on
-$2/code/bin/gvnix-dev script --file $2/code/addon-web-dialog/src/main/resources/dialog.roo
+gvnix.sh script --file $2/code/addon-web-dialog/src/main/resources/dialog.roo
 mvn test tomcat:run selenium:xvfb selenium:selenese -Dmaven.tomcat.fork=true 
 rm -rf /tmp/gvnixtest$1/*
 
 ## gvNIX flex add-on
-$2/code/bin/gvnix-dev script --file $2/code/spring-flex-roo/org.springframework.flex.roo.addon/src/test/resources/basic-flex-scaffold-test.roo
+gvnix.sh script --file $2/code/spring-flex-roo/org.springframework.flex.roo.addon/src/test/resources/basic-flex-scaffold-test.roo
 mvn test tomcat:run -Dmaven.tomcat.fork=true 
 rm -rf /tmp/gvnixtest$1/*
 
 ## gvNIX flex add-on
-$2/code/bin/gvnix-dev script --file $2/code/spring-flex-roo/org.springframework.flex.roo.addon/src/test/resources/remoting-scaffold-all-test.roo
+gvnix.sh script --file $2/code/spring-flex-roo/org.springframework.flex.roo.addon/src/test/resources/remoting-scaffold-all-test.roo
 mvn test tomcat:run -Dmaven.tomcat.fork=true 
 rm -rf /tmp/gvnixtest$1/*
 
 ## gvNIX flex add-on
-$2/code/bin/gvnix-dev script --file $2/code/spring-flex-roo/org.springframework.flex.roo.addon/src/main/resources/flex.roo
+gvnix.sh script --file $2/code/spring-flex-roo/org.springframework.flex.roo.addon/src/main/resources/flex.roo
 mvn test tomcat:run -Dmaven.tomcat.fork=true 
 rm -rf /tmp/gvnixtest$1/*
 
 ## gvNIX flex add-on
-$2/code/bin/gvnix-dev script --file $2/code/spring-flex-roo/rootunes.roo
+gvnix.sh script --file $2/code/spring-flex-roo/rootunes.roo
 mvn test tomcat:run -Dmaven.tomcat.fork=true 
 rm -rf /tmp/gvnixtest$1/*
 
 ## gvNIX i18n add-on
-$2/code/bin/gvnix-dev script --file $2/code/addon-web-i18n/src/main/resources/es-i18n.roo
+gvnix.sh script --file $2/code/addon-web-i18n/src/main/resources/es-i18n.roo
 mvn test tomcat:run selenium:xvfb selenium:selenese -Dmaven.tomcat.fork=true 
 rm -rf /tmp/gvnixtest$1/*
 
 ## gvNIX menu add-on
-$2/code/bin/gvnix-dev script --file $2/code/addon-web-menu/src/main/resources/menu.roo
+gvnix.sh script --file $2/code/addon-web-menu/src/main/resources/menu.roo
 mvn test tomcat:run selenium:xvfb selenium:selenese -Dmaven.tomcat.fork=true 
 rm -rf /tmp/gvnixtest$1/*
 
 ## gvNIX menu add-on
-$2/code/bin/gvnix-dev script --file $2/code/addon-web-menu/src/test/resources/base.roo
+gvnix.sh script --file $2/code/addon-web-menu/src/test/resources/base.roo
 mvn test tomcat:run selenium:xvfb selenium:selenese -Dmaven.tomcat.fork=true 
 rm -rf /tmp/gvnixtest$1/*
 
 ## gvNIX occ add-on
-$2/code/bin/gvnix-dev script --file $2/code/addon-occ/src/main/resources/occ.roo
+gvnix.sh script --file $2/code/addon-occ/src/main/resources/occ.roo
 mvn test tomcat:run selenium:xvfb selenium:selenese -Dmaven.tomcat.fork=true 
 rm -rf /tmp/gvnixtest$1/*
 
 ## gvNIX pattern add-on
-$2/code/bin/gvnix-dev script --file $2/code/addon-web-pattern/src/main/resources/pattern.roo
-$2/code/bin/gvnix-dev hint
+gvnix.sh script --file $2/code/addon-web-pattern/src/main/resources/pattern.roo
+gvnix.sh hint
 mvn test tomcat:run selenium:xvfb selenium:selenese -Dmaven.tomcat.fork=true 
 rm -rf /tmp/gvnixtest$1/*
 
 ## gvNIX pattern add-on
-$2/code/bin/gvnix-dev script --file $2/code/addon-web-pattern/src/test/resources/test-script-pkc-roo
-$2/code/bin/gvnix-dev hint
+gvnix.sh script --file $2/code/addon-web-pattern/src/test/resources/test-script-pkc-roo
+gvnix.sh hint
 mvn test tomcat:run selenium:xvfb selenium:selenese -Dmaven.tomcat.fork=true 
 rm -rf /tmp/gvnixtest$1/*
 
 ## gvNIX pattern add-on
-$2/code/bin/gvnix-dev script --file $2/code/addon-web-pattern/src/test/resources/test-script-pkc2-roo
-$2/code/bin/gvnix-dev hint
+gvnix.sh script --file $2/code/addon-web-pattern/src/test/resources/test-script-pkc2-roo
+gvnix.sh hint
 mvn test tomcat:run selenium:xvfb selenium:selenese -Dmaven.tomcat.fork=true 
 rm -rf /tmp/gvnixtest$1/*
 
 ## gvNIX pattern add-on
-$2/code/bin/gvnix-dev script --file $2/code/addon-web-pattern/src/test/resources/test-script-pkc3-roo
-$2/code/bin/gvnix-dev hint
+gvnix.sh script --file $2/code/addon-web-pattern/src/test/resources/test-script-pkc3-roo
+gvnix.sh hint
 mvn test tomcat:run selenium:xvfb selenium:selenese -Dmaven.tomcat.fork=true 
 rm -rf /tmp/gvnixtest$1/*
 
 ## gvNIX pattern add-on
-$2/code/bin/gvnix-dev script --file $2/code/addon-web-pattern/src/test/resources/test-script-manytomany-roo
-$2/code/bin/gvnix-dev hint
+gvnix.sh script --file $2/code/addon-web-pattern/src/test/resources/test-script-manytomany-roo
+gvnix.sh hint
 mvn test tomcat:run selenium:xvfb selenium:selenese -Dmaven.tomcat.fork=true 
 rm -rf /tmp/gvnixtest$1/*
 
 ## gvNIX report add-on
-$2/code/bin/gvnix-dev script --file $2/code/addon-web-report/src/main/resources/report.roo
+gvnix.sh script --file $2/code/addon-web-report/src/main/resources/report.roo
 mvn test tomcat:run selenium:xvfb selenium:selenese -Dmaven.tomcat.fork=true 
 rm -rf /tmp/gvnixtest$1/*
 
 ## gvNIX report add-on
-$2/code/bin/gvnix-dev script --file $2/code/addon-web-report/src/test/resources/gvnix-test.roo
+gvnix.sh script --file $2/code/addon-web-report/src/test/resources/gvnix-test.roo
 mvn test tomcat:run selenium:xvfb selenium:selenese -Dmaven.tomcat.fork=true 
 rm -rf /tmp/gvnixtest$1/*
 
 ## gvNIX service add-on
-$2/code/bin/gvnix-dev script --file $2/code/addon-service/src/main/resources/bing.roo
+gvnix.sh script --file $2/code/addon-service/src/main/resources/bing.roo
 mvn test tomcat:run selenium:xvfb selenium:selenese -Dmaven.tomcat.fork=true 
 rm -rf /tmp/gvnixtest$1/*
 
 ## gvNIX service add-on
-$2/code/bin/gvnix-dev script --file $2/code/addon-service/src/main/resources/service.roo
+gvnix.sh script --file $2/code/addon-service/src/main/resources/service.roo
 mvn test tomcat:run selenium:xvfb selenium:selenese -Dmaven.tomcat.fork=true 
 rm -rf /tmp/gvnixtest$1/*
 
 ## gvNIX service add-on
-$2/code/bin/gvnix-dev script --file $2/code/addon-service/src/test/resources/gvnix-test-no-web.roo
+gvnix.sh script --file $2/code/addon-service/src/test/resources/gvnix-test-no-web.roo
 mvn test tomcat:run selenium:xvfb selenium:selenese -Dmaven.tomcat.fork=true 
 rm -rf /tmp/gvnixtest$1/*
 
 ## gvNIX service add-on
-$2/code/bin/gvnix-dev script --file $2/code/addon-service/src/test/resources/gvnix-test.roo
+gvnix.sh script --file $2/code/addon-service/src/test/resources/gvnix-test.roo
 mvn test tomcat:run -Dmaven.tomcat.fork=true 
 rm -rf /tmp/gvnixtest$1/*
 
 ## gvNIX theme add-on 
-$2/code/bin/gvnix-dev script --file $2/code/addon-web-theme/src/main/resources/theme.roo
+gvnix.sh script --file $2/code/addon-web-theme/src/main/resources/theme.roo
 rm -rf /tmp/gvnixtest$1/*
 
 ## gvNIX add-ons
-$2/code/bin/gvnix-dev script --file $2/code/src/main/assembly/samples/gvnix-sample.roo
+gvnix.sh script --file $2/code/src/main/assembly/samples/gvnix-sample.roo
 mvn test tomcat:run selenium:xvfb selenium:selenese -Dmaven.tomcat.fork=true 
 rm -rf /tmp/gvnixtest$1/*
 
 ## gvNIX add-ons
-$2/code/bin/gvnix-dev script --file $2/code/src/test/resources/script.roo
+gvnix.sh script --file $2/code/src/test/resources/script.roo
 mvn test tomcat:run selenium:xvfb selenium:selenese -Dmaven.tomcat.fork=true 
 rm -rf /tmp/gvnixtest$1/*
 
 ## gvNIX add-ons
-$2/code/bin/gvnix-dev script --file $2/code/src/test/resources/tiendavirtual.roo
+gvnix.sh script --file $2/code/src/test/resources/tiendavirtual.roo
 mvn test tomcat:run selenium:xvfb selenium:selenese -Dmaven.tomcat.fork=true 
 rm -rf /tmp/gvnixtest$1/*
