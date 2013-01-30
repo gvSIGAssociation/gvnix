85e8df47e409f762170cd2f7be7d6de0db661625
mmartinez <mmartinez@69435866-3869-0410-973d-9f951a852354>
2013-01-30 11:06:19 +0000

    #8216 Revisar parámetros proporcionados al script de integración: directorio bin para arrancar y directorio src para acceder a los tests.
    git-svn-id: http://scmcit.gva.es/svn/gvnix/trunk/code@1713 69435866-3869-0410-973d-9f951a852354

--- src/test/resources/integration.sh
+++ src/test/resources/integration.sh
@@ -1,7 +1,11 @@
 
-## Test run from hudson integration system.
+## Run all integration tests from console and form jenkins integration system daily.
 ## Run example:
-##  ${WORKSPACE}/code/src/test/resources/integration.sh ${BUILD_NUMBER} ${WORKSPACE} 
+##  ${WORKSPACE}/code/src/test/resources/integration.sh ${BINARY_PATH} ${SOURCE_PATH}
+## Where input parameters are:
+##  * BINARY_PATH: Path to gvNIX distribution bin folder with which execute the tests 
+##  * SOURCE_PATH: Path to gvNIX root source folder for access the test scripts
+## Output execution tests will be available at /tmp/gvnixtest folder
 
 ##
 ## Initial
@@ -12,9 +16,9 @@
 	export DISPLAY=:1
 	
 	## Remove old potential temporal folder of previous test, create new one and go into
-	rm -rf /tmp/gvnixtest*
-	mkdir /tmp/gvnixtest$1
-	cd /tmp/gvnixtest$1
+	rm -rf /tmp/gvnixtest
+	mkdir /tmp/gvnixtest
+	cd /tmp/gvnixtest
 
 ##
 ## Roo
@@ -24,7 +28,7 @@
 	echo bikeshop
 	mkdir bikeshop
 	cd bikeshop
-	gvnix.sh script --file bikeshop.roo
+	$2/gvnix.sh script --file bikeshop.roo
 	mvn test tomcat:run -Dmaven.tomcat.fork=true
 	cd .. 
 	
@@ -32,7 +36,7 @@
 	echo clinic
 	mkdir clinic
 	cd clinic
-	gvnix.sh script --file clinic.roo
+	$2/gvnix.sh script --file clinic.roo
 	mvn test tomcat:run selenium:xvfb selenium:selenese -Dmaven.tomcat.fork=true 
 	cd ..
 	
@@ -40,7 +44,7 @@
 	echo embedding
 	mkdir embedding
 	cd embedding
-	gvnix.sh script --file embedding.roo
+	$2/gvnix.sh script --file embedding.roo
 	mvn test tomcat:run -Dmaven.tomcat.fork=true 
 	cd ..
 	
@@ -48,7 +52,7 @@
 	echo expenses
 	mkdir expenses
 	cd expenses
-	gvnix.sh script --file expenses.roo
+	$2/gvnix.sh script --file expenses.roo
 	mvn test tomcat:run -Dmaven.tomcat.fork=true 
 	cd ..
 	
@@ -56,7 +60,7 @@
 	echo gae-expenses
 	mkdir gae-expenses
 	cd gae-expenses
-	gvnix.sh script --file gae-expenses.roo
+	$2/gvnix.sh script --file gae-expenses.roo
 	mvn test tomcat:run -Dmaven.tomcat.fork=true 
 	cd ..
 	
@@ -64,7 +68,7 @@
 	echo multimodule
 	mkdir multimodule
 	cd multimodule
-	gvnix.sh script --file multimodule.roo
+	$2/gvnix.sh script --file multimodule.roo
 	mvn test tomcat:run -Dmaven.tomcat.fork=true 
 	cd ..
 	
@@ -72,7 +76,7 @@
 	echo pizzashop
 	mkdir pizzashop
 	cd pizzashop
-	gvnix.sh script --file pizzashop.roo
+	$2/gvnix.sh script --file pizzashop.roo
 	mvn test tomcat:run -Dmaven.tomcat.fork=true 
 	cd ..
 	
@@ -80,7 +84,7 @@
 	echo vote
 	mkdir vote
 	cd vote
-	gvnix.sh script --file vote.roo
+	$2/gvnix.sh script --file vote.roo
 	mvn test tomcat:run -Dmaven.tomcat.fork=true 
 	cd ..
 	
@@ -88,7 +92,7 @@
 	echo wedding
 	mkdir wedding
 	cd wedding
-	gvnix.sh script --file wedding.roo
+	$2/gvnix.sh script --file wedding.roo
 	mvn test tomcat:run selenium:xvfb selenium:selenese -Dmaven.tomcat.fork=true 
 	cd ..
 
@@ -100,7 +104,7 @@
 	echo binding
 	mkdir binding
 	cd binding
-	gvnix.sh script --file $2/code/addon-web-mvc-binding/src/main/resources/binding.roo
+	$2/gvnix.sh script --file $2/code/addon-web-mvc-binding/src/main/resources/binding.roo
 	mvn test tomcat:run selenium:xvfb selenium:selenese -Dmaven.tomcat.fork=true 
 	cd ..
 
@@ -112,7 +116,7 @@
 	echo configuration
 	mkdir configuration
 	cd configuration
-	gvnix.sh script --file $2/code/addon-dynamic-configuration/src/main/resources/configuration.roo
+	$2/gvnix.sh script --file $2/code/addon-dynamic-configuration/src/main/resources/configuration.roo
 	mvn test tomcat:run selenium:xvfb selenium:selenese -Dmaven.tomcat.fork=true 
 	cd ..
 
@@ -124,7 +128,7 @@
 	echo dialog
 	mkdir dialog
 	cd dialog
-	gvnix.sh script --file $2/code/addon-web-dialog/src/main/resources/dialog.roo
+	$2/gvnix.sh script --file $2/code/addon-web-dialog/src/main/resources/dialog.roo
 	mvn test tomcat:run selenium:xvfb selenium:selenese -Dmaven.tomcat.fork=true 
 	cd ..
 
@@ -136,7 +140,7 @@
 	echo basic-flex-scaffold-test
 	mkdir basic-flex-scaffold-test
 	cd basic-flex-scaffold-test
-	gvnix.sh script --file $2/code/spring-flex-roo/org.springframework.flex.roo.addon/src/test/resources/basic-flex-scaffold-test.roo
+	$2/gvnix.sh script --file $2/code/spring-flex-roo/org.springframework.flex.roo.addon/src/test/resources/basic-flex-scaffold-test.roo
 	mvn test tomcat:run -Dmaven.tomcat.fork=true 
 	cd ..
 	
@@ -144,7 +148,7 @@
 	echo remoting-scaffold-all-test
 	mkdir remoting-scaffold-all-test
 	cd remoting-scaffold-all-test
-	gvnix.sh script --file $2/code/spring-flex-roo/org.springframework.flex.roo.addon/src/test/resources/remoting-scaffold-all-test.roo
+	$2/gvnix.sh script --file $2/code/spring-flex-roo/org.springframework.flex.roo.addon/src/test/resources/remoting-scaffold-all-test.roo
 	mvn test tomcat:run -Dmaven.tomcat.fork=true 
 	cd ..
 	
@@ -152,7 +156,7 @@
 	echo flex
 	mkdir flex
 	cd flex
-	gvnix.sh script --file $2/code/spring-flex-roo/org.springframework.flex.roo.addon/src/main/resources/flex.roo
+	$2/gvnix.sh script --file $2/code/spring-flex-roo/org.springframework.flex.roo.addon/src/main/resources/flex.roo
 	mvn test tomcat:run -Dmaven.tomcat.fork=true 
 	cd ..
 	
@@ -160,7 +164,7 @@
 	echo rootunes
 	mkdir rootunes
 	cd rootunes
-	gvnix.sh script --file $2/code/spring-flex-roo/rootunes.roo
+	$2/gvnix.sh script --file $2/code/spring-flex-roo/rootunes.roo
 	mvn test tomcat:run -Dmaven.tomcat.fork=true 
 	cd ..
 
@@ -172,7 +176,7 @@
 	echo es-i18n
 	mkdir es-i18n
 	cd es-i18n
-	gvnix.sh script --file $2/code/addon-web-i18n/src/main/resources/es-i18n.roo
+	$2/gvnix.sh script --file $2/code/addon-web-i18n/src/main/resources/es-i18n.roo
 	mvn test tomcat:run selenium:xvfb selenium:selenese -Dmaven.tomcat.fork=true 
 	cd ..
 
@@ -184,7 +188,7 @@
 	echo menu
 	mkdir menu
 	cd menu
-	gvnix.sh script --file $2/code/addon-web-menu/src/main/resources/menu.roo
+	$2/gvnix.sh script --file $2/code/addon-web-menu/src/main/resources/menu.roo
 	mvn test tomcat:run selenium:xvfb selenium:selenese -Dmaven.tomcat.fork=true 
 	cd ..
 	
@@ -192,7 +196,7 @@
 	echo base
 	mkdir base
 	cd base
-	gvnix.sh script --file $2/code/addon-web-menu/src/test/resources/base.roo
+	$2/gvnix.sh script --file $2/code/addon-web-menu/src/test/resources/base.roo
 	mvn test tomcat:run -Dmaven.tomcat.fork=true 
 	cd ..
 
@@ -204,7 +208,7 @@
 	echo occ
 	mkdir occ
 	cd occ
-	gvnix.sh script --file $2/code/addon-occ/src/main/resources/occ.roo
+	$2/gvnix.sh script --file $2/code/addon-occ/src/main/resources/occ.roo
 	mvn test tomcat:run selenium:xvfb selenium:selenese -Dmaven.tomcat.fork=true 
 	cd ..
 
@@ -216,8 +220,8 @@
 	echo pattern
 	mkdir pattern
 	cd pattern
-	gvnix.sh script --file $2/code/addon-web-pattern/src/main/resources/pattern.roo
-	gvnix.sh hint
+	$2/gvnix.sh script --file $2/code/addon-web-pattern/src/main/resources/pattern.roo
+	$2/gvnix.sh hint
 	mvn test tomcat:run selenium:xvfb selenium:selenese -Dmaven.tomcat.fork=true 
 	cd ..
 	
@@ -225,8 +229,8 @@
 	echo test-script-pkc-roo
 	mkdir test-script-pkc-roo
 	cd test-script-pkc-roo
-	gvnix.sh script --file $2/code/addon-web-pattern/src/test/resources/test-script-pkc-roo
-	gvnix.sh hint
+	$2/gvnix.sh script --file $2/code/addon-web-pattern/src/test/resources/test-script-pkc-roo
+	$2/gvnix.sh hint
 	mvn test tomcat:run selenium:xvfb selenium:selenese -Dmaven.tomcat.fork=true 
 	cd ..
 	
@@ -234,8 +238,8 @@
 	echo test-script-pkc2-roo
 	mkdir test-script-pkc2-roo
 	cd test-script-pkc2-roo
-	gvnix.sh script --file $2/code/addon-web-pattern/src/test/resources/test-script-pkc2-roo
-	gvnix.sh hint
+	$2/gvnix.sh script --file $2/code/addon-web-pattern/src/test/resources/test-script-pkc2-roo
+	$2/gvnix.sh hint
 	mvn test tomcat:run selenium:xvfb selenium:selenese -Dmaven.tomcat.fork=true 
 	cd ..
 	
@@ -243,8 +247,8 @@
 	echo test-script-pkc3-roo
 	mkdir test-script-pkc3-roo
 	cd test-script-pkc3-roo
-	gvnix.sh script --file $2/code/addon-web-pattern/src/test/resources/test-script-pkc3-roo
-	gvnix.sh hint
+	$2/gvnix.sh script --file $2/code/addon-web-pattern/src/test/resources/test-script-pkc3-roo
+	$2/gvnix.sh hint
 	mvn test tomcat:run selenium:xvfb selenium:selenese -Dmaven.tomcat.fork=true 
 	cd ..
 	
@@ -252,8 +256,8 @@
 	echo test-script-manytomany-roo
 	mkdir test-script-manytomany-roo
 	cd test-script-manytomany-roo
-	gvnix.sh script --file $2/code/addon-web-pattern/src/test/resources/test-script-manytomany-roo
-	gvnix.sh hint
+	$2/gvnix.sh script --file $2/code/addon-web-pattern/src/test/resources/test-script-manytomany-roo
+	$2/gvnix.sh hint
 	mvn test tomcat:run selenium:xvfb selenium:selenese -Dmaven.tomcat.fork=true 
 	cd ..
 
@@ -265,7 +269,7 @@
 	echo report
 	mkdir report
 	cd report
-	gvnix.sh script --file $2/code/addon-web-report/src/main/resources/report.roo
+	$2/gvnix.sh script --file $2/code/addon-web-report/src/main/resources/report.roo
 	mvn test tomcat:run selenium:xvfb selenium:selenese -Dmaven.tomcat.fork=true 
 	cd ..
 	
@@ -273,7 +277,7 @@
 	echo gvnix-test
 	mkdir gvnix-test
 	cd gvnix-test
-	gvnix.sh script --file $2/code/addon-web-report/src/test/resources/gvnix-test.roo
+	$2/gvnix.sh script --file $2/code/addon-web-report/src/test/resources/gvnix-test.roo
 	mvn test tomcat:run -Dmaven.tomcat.fork=true 
 	cd ..
 
@@ -285,7 +289,7 @@
 	echo bing
 	mkdir bing
 	cd bing
-	gvnix.sh script --file $2/code/addon-service/src/main/resources/bing.roo
+	$2/gvnix.sh script --file $2/code/addon-service/src/main/resources/bing.roo
 	mvn test tomcat:run selenium:xvfb selenium:selenese -Dmaven.tomcat.fork=true 
 	cd ..
 	
@@ -293,7 +297,7 @@
 	echo service
 	mkdir service
 	cd service
-	gvnix.sh script --file $2/code/addon-service/src/main/resources/service.roo
+	$2/gvnix.sh script --file $2/code/addon-service/src/main/resources/service.roo
 	mvn test tomcat:run selenium:xvfb selenium:selenese -Dmaven.tomcat.fork=true 
 	cd ..
 	
@@ -301,7 +305,7 @@
 	echo gvnix-test-no-web
 	mkdir gvnix-test-no-web
 	cd gvnix-test-no-web
-	gvnix.sh script --file $2/code/addon-service/src/test/resources/gvnix-test-no-web.roo
+	$2/gvnix.sh script --file $2/code/addon-service/src/test/resources/gvnix-test-no-web.roo
 	mvn test tomcat:run selenium:xvfb selenium:selenese -Dmaven.tomcat.fork=true 
 	cd ..
 	
@@ -309,7 +313,7 @@
 	echo gvnix-test
 	mkdir gvnix-test
 	cd gvnix-test
-	gvnix.sh script --file $2/code/addon-service/src/test/resources/gvnix-test.roo
+	$2/gvnix.sh script --file $2/code/addon-service/src/test/resources/gvnix-test.roo
 	mvn test tomcat:run -Dmaven.tomcat.fork=true 
 	cd ..
 
@@ -321,7 +325,7 @@
 	echo theme
 	mkdir theme
 	cd theme
-	gvnix.sh script --file $2/code/addon-web-theme/src/main/resources/theme.roo
+	$2/gvnix.sh script --file $2/code/addon-web-theme/src/main/resources/theme.roo
 	mvn test tomcat:run selenium:xvfb selenium:selenese -Dmaven.tomcat.fork=true 
 	cd ..
 
@@ -333,7 +337,7 @@
 	echo gvnix-sample
 	mkdir gvnix-sample
 	cd gvnix-sample
-	gvnix.sh script --file $2/code/src/main/assembly/samples/gvnix-sample.roo
+	$2/gvnix.sh script --file $2/code/src/main/assembly/samples/gvnix-sample.roo
 	mvn test tomcat:run -Dmaven.tomcat.fork=true 
 	cd ..
 	
@@ -341,7 +345,7 @@
 	echo script
 	mkdir script
 	cd script
-	gvnix.sh script --file $2/code/src/test/resources/script.roo
+	$2/gvnix.sh script --file $2/code/src/test/resources/script.roo
 	mvn test tomcat:run selenium:xvfb selenium:selenese -Dmaven.tomcat.fork=true 
 	cd ..
 	
@@ -349,6 +353,6 @@
 	echo tiendavirtual
 	mkdir tiendavirtual
 	cd tiendavirtual
-	gvnix.sh script --file $2/code/src/test/resources/tiendavirtual.roo
+	$2/gvnix.sh script --file $2/code/src/test/resources/tiendavirtual.roo
 	mvn test tomcat:run selenium:xvfb selenium:selenese -Dmaven.tomcat.fork=true 
 	cd ..
