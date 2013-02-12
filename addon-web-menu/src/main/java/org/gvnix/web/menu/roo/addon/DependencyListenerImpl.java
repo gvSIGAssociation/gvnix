d08171412dcace0f258edcb69edef687ad6015fa
jmvivo <jmvivo@69435866-3869-0410-973d-9f951a852354>
2013-02-12 15:03:27 +0000

    #7933 Arreglado el listener que gestiona cuando se añade la dependencia con Spring Security
    git-svn-id: http://scmcit.gva.es/svn/gvnix/trunk/code@1788 69435866-3869-0410-973d-9f951a852354

--- addon-web-menu/src/main/java/org/gvnix/web/menu/roo/addon/DependencyListenerImpl.java
+++ addon-web-menu/src/main/java/org/gvnix/web/menu/roo/addon/DependencyListenerImpl.java
@@ -18,62 +18,67 @@
  */
 package org.gvnix.web.menu.roo.addon;
 
+import java.util.logging.Logger;
+
+import org.apache.commons.lang3.ObjectUtils;
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.Service;
+import org.osgi.service.component.ComponentContext;
+import org.springframework.roo.metadata.MetadataDependencyRegistry;
 import org.springframework.roo.metadata.MetadataNotificationListener;
-import org.springframework.roo.project.Dependency;
+import org.springframework.roo.project.ProjectMetadata;
+import org.springframework.roo.support.logging.HandlerUtils;
 
 /**
- * Dependency listener to be notified of changes to project dependencies.
+ * Check for Spring Security dependency changes to update menu artifacts.
  * 
  * @author Enrique Ruiz( eruiz at disid dot com ) at <a
  *         href="http://www.disid.com">DiSiD Technologies S.L.</a> made for <a
  *         href="http://www.cit.gva.es">Conselleria d'Infraestructures i
  *         Transport</a>
  */
-@Component
+@Component(immediate = true)
 @Service
 public class DependencyListenerImpl implements MetadataNotificationListener {
 
-    public void notify(String upstreamDependency, String downstreamDependency) {
-        // Required when implements change from DependencyListener to
-        // MetadataNotificationListener
-        // TODO Is required now some extra code here ?
-    }
+    private static final Logger LOGGER = HandlerUtils
+            .getLogger(DependencyListenerImpl.class);
+
+    @Reference private MetadataDependencyRegistry metadataDependencyRegistry;
 
     /**
      * Use PageOperations to execute operations
      */
     @Reference private MenuEntryOperations operations;
 
-    /**
-     * If Spring Security was added manually, update web artifacts to use secure
-     * tags.
-     * <p>
-     * TODO: Unit tests
-     * 
-     * @param d
-     */
-    public void dependencyAdded(Dependency d) {
-        if ("spring-security-core".equals(d.getArtifactId())
-                && "org.springframework.security".equals(d.getGroupId())) {
-            operations.createWebArtefacts("~.web.menu");
-        }
+    private Boolean hasSpringSecurity = null;
+
+    protected void activate(ComponentContext context) {
+        this.metadataDependencyRegistry.addNotificationListener(this);
+    }
+
+    protected void deactivate(ComponentContext context) {
+        this.metadataDependencyRegistry.removeNotificationListener(this);
     }
 
     /**
-     * If Spring Security was removed manually, update web artifacts to use
-     * unsecure tags.
-     * <p>
-     * TODO: Unit tests
-     * 
-     * @param d
+     * {@inheritDoc} When Project metadata changes look for SpringSecurity
+     * dependency and update menu artifacts.
      */
-    public void dependencyRemoved(Dependency d) {
-        if ("spring-security-core".equals(d.getArtifactId())
-                && "org.springframework.security".equals(d.getGroupId())) {
-            operations.createWebArtefacts("~.web.menu");
+    public void notify(String upstreamDependency, String downstreamDependency) {
+        // Check if is project metadata
+        if (ProjectMetadata.isValid(upstreamDependency)) {
+            if (operations.isGvNixMenuAvailable()) {
+                // if dependency changes or its first call
+                if (!ObjectUtils.equals(operations.isSpringSecurityInstalled(),
+                        hasSpringSecurity)) {
+                    LOGGER.finest("Spring Security changed or startup");
+                    // update hasSpringSecurity variable and update artifacts
+                    hasSpringSecurity = operations.isSpringSecurityInstalled();
+                    operations.createWebArtefacts("~.web.menu");
+                }
+            }
         }
     }
 }
