9a00b9938d209f357a3c12295a01ed3f0a816e64
jmvivo <jmvivo@69435866-3869-0410-973d-9f951a852354>
2013-03-13 11:27:18 +0000

    Arreglado error de ruta del jar
    git-svn-id: http://scmcit.gva.es/svn/gvnix/trunk/code@1892 69435866-3869-0410-973d-9f951a852354

--- wrapping/postgresql/jdbc3/lib/install-lib.sh
+++ wrapping/postgresql/jdbc3/lib/install-lib.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mvn install:install-file -Dfile=lib/postgresql-9.1-902.jdbc3.jar -DgroupId=postgresql -DartifactId=postgresql -Dversion=9.1-902.jdbc3 -Dpackaging=jar 
+mvn install:install-file -Dfile=postgresql-9.1-902.jdbc3.jar -DgroupId=postgresql -DartifactId=postgresql -Dversion=9.1-902.jdbc3 -Dpackaging=jar 
