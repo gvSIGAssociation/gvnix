3dfc08aea008e88192366700b19d9e9fd9d21214
mmartinez <mmartinez@69435866-3869-0410-973d-9f951a852354>
2013-04-17 09:05:19 +0000

    #9048 Borrar los módulos addon-weblayer, dynamiclist, addon-web-mvc y addon-database-sequence-id-generator. Mover directorio contenido del directorio samples a src/test/resources.
    git-svn-id: http://scmcit.gva.es/svn/gvnix/trunk/code@1962 69435866-3869-0410-973d-9f951a852354

--- dynamiclist/src/resources/scripts/DYNAMICLIST_CONFIG_data.sql
+++
@@ -1,4 +0,0 @@
-
-
-INSERT INTO GLOBAL_CONFIG(ID, ENTITY, ENTITY_PROPERTIES, HIDDEN_PROPERTIES, ORDERBY, WHEREFILTER_FIX, INFOFILTER_FIX) 
-   VALUES(1, 'PizzaOrder', ' id,address, name ,  total', 'version', null, 'id > 0', null);
