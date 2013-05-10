d76cd2253c6c4d90c4f2dc6dc09a49d41535bdf4
jmvivo <jmvivo@69435866-3869-0410-973d-9f951a852354>
2013-05-10 08:51:56 +0000

    Refs #9487 created base for add-on gvnix web mvc
    git-svn-id: http://scmcit.gva.es/svn/gvnix/trunk/code@2025 69435866-3869-0410-973d-9f951a852354

---
+++ addon-web-mvc/src/main/java/org/gvnix/addon/web/mvc/MvcOperationsImpl.java
@@ -0,0 +1,78 @@
+/*
+ * gvNIX. Spring Roo based RAD tool for Generalitat Valenciana Copyright (C)
+ * 2013 Generalitat Valenciana
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see &lt;http://www.gnu.org/copyleft/gpl.html&gt;.
+ */
+package org.gvnix.addon.web.mvc;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Reference;
+import org.apache.felix.scr.annotations.Service;
+import org.gvnix.support.dependenciesmanager.DependenciesVersionManager;
+import org.springframework.roo.metadata.MetadataService;
+import org.springframework.roo.project.FeatureNames;
+import org.springframework.roo.project.ProjectOperations;
+import org.springframework.roo.project.Repository;
+import org.springframework.roo.support.util.XmlUtils;
+import org.w3c.dom.Element;
+
+/**
+ * Implementation of operations this add-on offers.
+ * 
+ * @since 1.1
+ */
+@Component
+// Use these Apache Felix annotations to register your commands class in the Roo
+// container
+@Service
+public class MvcOperationsImpl implements MvcOperations {
+
+    @Reference private ProjectOperations projectOperations;
+
+    @Reference private MetadataService metadataService;
+
+    /** {@inheritDoc} */
+    public boolean isSetupAvailable() {
+        // Check if JPA is installed
+        return projectOperations
+                .isFeatureInstalledInFocusedModule(FeatureNames.MVC)
+                && !projectOperations
+                        .isFeatureInstalledInFocusedModule(MvcFeature.NAME);
+    }
+
+    public void setup() {
+        // Get add-on configuration file
+        Element configuration = XmlUtils.getConfiguration(getClass());
+
+        // Install the add-on repository needed
+        List<Element> repos = XmlUtils.findElements(
+                "/configuration/gvnix/repositories/repository", configuration);
+        for (Element repo : repos) {
+            projectOperations.addRepositories(
+                    projectOperations.getFocusedModuleName(),
+                    Collections.singleton(new Repository(repo)));
+        }
+
+        // Install dependencies
+        List<Element> depens = XmlUtils.findElements(
+                "/configuration/gvnix/dependencies/dependency", configuration);
+
+        DependenciesVersionManager.manageDependencyVersion(metadataService,
+                projectOperations, depens);
+    }
+}
