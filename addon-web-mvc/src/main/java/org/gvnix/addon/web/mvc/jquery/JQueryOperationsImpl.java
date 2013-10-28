d6a533726ecee096ee6bc0e9046ae10d8634a5ad
jmvivo <jmvivo@69435866-3869-0410-973d-9f951a852354>
2013-10-28 12:30:02 +0000

    Add additional null check
    git-svn-id: http://scmcit.gva.es/svn/gvnix/trunk/code@2223 69435866-3869-0410-973d-9f951a852354

--- addon-web-mvc/src/main/java/org/gvnix/addon/web/mvc/jquery/JQueryOperationsImpl.java
+++ addon-web-mvc/src/main/java/org/gvnix/addon/web/mvc/jquery/JQueryOperationsImpl.java
@@ -1,17 +1,17 @@
 /*
- * gvNIX. Spring Roo based RAD tool for Generalitat Valenciana     
+ * gvNIX. Spring Roo based RAD tool for Generalitat Valenciana
  * Copyright (C) 2013 Generalitat Valenciana
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/copyleft/gpl.html>.
  */
@@ -219,7 +219,7 @@ public class JQueryOperationsImpl extends AbstractOperations implements
         Validate.isTrue(controllerPath != null && !controllerPath.isEmpty(),
                 "Path is not specified in the @RooWebScaffold annotation for '"
                         .concat(controller.getSimpleTypeName()).concat("'"));
-        if (!controllerPath.startsWith("/")) {
+        if (controllerPath != null && !controllerPath.startsWith("/")) {
             controllerPath = "/".concat(controllerPath);
         }
 
