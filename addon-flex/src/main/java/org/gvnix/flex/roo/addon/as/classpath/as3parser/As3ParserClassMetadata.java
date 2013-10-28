d8f617262053e670abca7373ccce59efd1744ffb
jmvivo <jmvivo@69435866-3869-0410-973d-9f951a852354>
2013-10-28 12:30:36 +0000

    Catch Exception instead Throwable
    git-svn-id: http://scmcit.gva.es/svn/gvnix/trunk/code@2224 69435866-3869-0410-973d-9f951a852354

--- addon-flex/src/main/java/org/gvnix/flex/roo/addon/as/classpath/as3parser/As3ParserClassMetadata.java
+++ addon-flex/src/main/java/org/gvnix/flex/roo/addon/as/classpath/as3parser/As3ParserClassMetadata.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2002-2010 the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -90,7 +90,7 @@ public class As3ParserClassMetadata extends AbstractMetadataItem implements
                 logger.finest("Parsed '" + metadataIdentificationString + "'");
             }
         }
-        catch (Throwable ex) {
+        catch (Exception ex) {
             // non-fatal, it just means the type could not be parsed
 
             // TODO - Is there any type of ActionScript file that this parser
