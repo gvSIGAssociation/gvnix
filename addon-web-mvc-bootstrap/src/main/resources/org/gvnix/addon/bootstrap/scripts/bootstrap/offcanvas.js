a6f38bf4c1af9d73c3944cde70c04c0848e8d1e0
pargudo <pargudo@69435866-3869-0410-973d-9f951a852354>
2014-08-04 13:13:04 +0000

    Layouts available at 100% page width and/or sidebar. New tag for menu in sidebar opening toggle type.
    git-svn-id: http://scmcit.gva.es/svn/gvnix/trunk/code@2544 69435866-3869-0410-973d-9f951a852354

--- addon-web-mvc-bootstrap/src/main/resources/org/gvnix/addon/bootstrap/scripts/bootstrap/offcanvas.js
+++ addon-web-mvc-bootstrap/src/main/resources/org/gvnix/addon/bootstrap/scripts/bootstrap/offcanvas.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
   $('[data-toggle=offcanvas]').click(function() {
     $('.row-offcanvas').toggleClass('active');
+    $('#sidebar-wrapper').removeClass('affix');
   });
 });
+
