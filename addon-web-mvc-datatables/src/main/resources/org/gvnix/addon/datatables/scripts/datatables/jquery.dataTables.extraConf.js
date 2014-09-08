b7cb48142c5a7d4891c7d1279fdfe2a65f26d47b
jcgarcia <jcgarcia@69435866-3869-0410-973d-9f951a852354>
2014-09-08 08:12:58 +0000

    Refs #14624 - Updating datatables extra conf to save Datatable status on localStorage
    git-svn-id: http://scmcit.gva.es/svn/gvnix/trunk/code@2575 69435866-3869-0410-973d-9f951a852354

--- addon-web-mvc-datatables/src/main/resources/org/gvnix/addon/datatables/scripts/datatables/jquery.dataTables.extraConf.js
+++ addon-web-mvc-datatables/src/main/resources/org/gvnix/addon/datatables/scripts/datatables/jquery.dataTables.extraConf.js
@@ -5,6 +5,43 @@
 {
 	
 	"fnStateSave": function(oSettings, oData) {
+		// Saving oSettings on oData
+		oData.oSettings = {};
+		// Creating aoColumns
+		oData.oSettings.aoColumns = oSettings.aoColumns;
+		// Adding iDraw
+		oData.oSettings.iDraw = oSettings.iDraw;
+		// Adding _iDisplayStart
+		oData.oSettings._iDisplayStart = oSettings._iDisplayStart;
+		// Adding _iDisplayLength
+		oData.oSettings._iDisplayLength = oSettings._iDisplayLength;
+		// Adding oFeatures
+		oData.oSettings.oFeatures = oSettings.oFeatures;
+		// Adding oPreviousSearch
+		oData.oSettings.oPreviousSearch = oSettings.oPreviousSearch;
+		// Adding aoPreSearchCols
+		oData.oSettings.aoPreSearchCols = oSettings.aoPreSearchCols;
+		// Adding aaSortingFixed
+		oData.oSettings.aaSortingFixed = oSettings.aaSortingFixed;
+		// Adding aaSorting
+		oData.oSettings.aaSorting = oSettings.aaSorting;
+		
+		// Saving if is filtered
+		var generalSearch = oData.oSearch.sSearch;
+		var filteredByColumn = false;
+		for(i in oSettings.aoPreSearchCols){
+			if(oSettings.aoPreSearchCols[i].sSearch !== ""){
+				filteredByColumn = true;
+				break;
+			}
+		}
+		
+		if(generalSearch !== "" || filteredByColumn){
+			oData.isFiltered = true;
+		}else{
+			oData.isFiltered = false;
+		}
+		
 		// Generating hash location
 		var hashLocation = fnGetHashCode(window.location.pathname);
 		// Getting statePrefix
