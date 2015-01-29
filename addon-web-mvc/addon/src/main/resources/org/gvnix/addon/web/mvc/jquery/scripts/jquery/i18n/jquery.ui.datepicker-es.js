5827569fae441d3bb5e648c2b0fa2e1a8b4985f2
jcgarcia <jcgarcia@69435866-3869-0410-973d-9f951a852354>
2015-01-29 14:41:32 +0000

    Refs #15008 - Restructuring Addon Web MVC to generate annotations and addon jar
    git-svn-id: http://scmcit.gva.es/svn/gvnix/trunk/code@2839 69435866-3869-0410-973d-9f951a852354

---
+++ addon-web-mvc/addon/src/main/resources/org/gvnix/addon/web/mvc/jquery/scripts/jquery/i18n/jquery.ui.datepicker-es.js
@@ -0,0 +1,23 @@
+/* Inicialización en español para la extensión 'UI date picker' para jQuery. */
+/* Traducido por Vester (xvester@gmail.com). */
+jQuery(function($){
+	$.datepicker.regional['es'] = {
+		closeText: 'Cerrar',
+		prevText: '&#x3C;Ant',
+		nextText: 'Sig&#x3E;',
+		currentText: 'Hoy',
+		monthNames: ['enero','febrero','marzo','abril','mayo','junio',
+		'julio','agosto','septiembre','octubre','noviembre','diciembre'],
+		monthNamesShort: ['ene','feb','mar','abr','may','jun',
+		'jul','ago','sep','oct','nov','dic'],
+		dayNames: ['Domingo','Lunes','Martes','Miércoles','Jueves','Viernes','Sábado'],
+		dayNamesShort: ['Dom','Lun','Mar','Mié','Juv','Vie','Sáb'],
+		dayNamesMin: ['Do','Lu','Ma','Mi','Ju','Vi','Sá'],
+		weekHeader: 'Sm',
+		dateFormat: 'dd/mm/yy',
+		firstDay: 1,
+		isRTL: false,
+		showMonthAfterYear: false,
+		yearSuffix: ''};
+	$.datepicker.setDefaults($.datepicker.regional['es']);
+});
