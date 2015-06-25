e28f349bd074487c8a677d70d87039b94251eb76
Juan Carlos García <jugade92@gmail.com>
2015-06-25 10:33:59 +0200

    Adding new script to Travis CI

---
+++ deployment-support/build-travis.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+# Script to download Spring Roo .zip Distribution
+
+## Exit script on any error
+set -e 
+
+## Get current file abs_path
+function abs_path() {
+    pushd . > /dev/null;
+    if [ -d "$1" ]; then
+      cd "$1";
+      dirs -l +0;
+    else
+      cd "`dirname \"$1\"`";
+      cur_dir=`dirs -l +0`;
+      if [ "$cur_dir" == "/" ]; then
+        echo "$cur_dir`basename \"$1\"`";
+      else
+        echo "$cur_dir/`basename \"$1\"`";
+     fi;
+   fi;
+   popd > /dev/null;
+}
+THIS_FILE=$(abs_path $0)
+
+
+GVNIX_DEPLOYMENT_SUPPORT_DIR=`dirname $THIS_FILE`
+GVNIX_HOME=`dirname $GVNIX_DEPLOYMENT_SUPPORT_DIR`
+
+
+## Load utils functions
+source $GVNIX_DEPLOYMENT_SUPPORT_DIR/build-util.functions
+
+# Gets gvNIX version from first version tag on root pom.xml
+GVNIX_VERSION=`grep "[<]version[>]\K([^<]*)" $GVNIX_HOME/pom.xml -oPm1`
+# Gets roo version from first version tag on root pom.xml
+ROO_VERSION=`grep "[<]roo.version[>]\K([^<]*)" $GVNIX_HOME/pom.xml -oPm1`
+
+# Downloading ROO
+mkdir roo-distribution;
+wget -O roo-distribution/spring-roo-"$ROO_VERSION".zip http://spring-roo-repository.springsource.org.s3.amazonaws.com/milestone/ROO/spring-roo-"$ROO_VERSION".zip
